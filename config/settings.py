d0dc707423b73675aa9e043f30e6c4c9bd6c700b
Phuoc Nguyen <phuoc.nguyenhoang@hdwebsoft.dev>
2025-07-23 09:40:54 +0700

    init project

--- config/settings.py
+++ config/settings.py
@@ -10,10 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(BASE_DIR / "apps"))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'users',
+    'blog',
+    'notifications',
+    'api',
+    'core',
 ]
 
 MIDDLEWARE = [
